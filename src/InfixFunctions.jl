6dd4aa0937ee95820a772bd5570c7139ca2119fb
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2017-11-03 21:37:02 -0600

    Update InfixFunctions.jl

--- src/InfixFunctions.jl
+++ src/InfixFunctions.jl
@@ -1,5 +1,60 @@
+__precompile__()
+
 module InfixFunctions
 
-# package code goes here
+using MacroTools: @capture
+
+export @infix
+
+
+struct InfixFunction <: Function
+    operator::Function
+end
+
+(infix::InfixFunction)(arg₁, arg₂) = infix.operator(arg₁, arg₂)
+
+function Base.:|(arg₁, infix::InfixFunction)
+    return InfixFunction(arg₂ -> infix.operator(arg₁, arg₂))
+end
+
+Base.:|(infix::InfixFunction, arg₂) = infix.operator(arg₂)
+
+"""
+    @infix function_expr
+
+# Usage
+
+```julia
+julia> add = @infix (x, y) -> x + y
+(::InfixFunction) (generic function with 1 method)
+
+julia> 5 |add| 5
+10
+
+julia> sub = @infix function (x, y)
+           return x - y
+       end
+(::InfixFunction) (generic function with 1 method)
+
+julia> 5 |sub| 5
+0
+```
+
+"""
+macro infix(operator)
+    @capture(
+        operator,
+
+        function (arg₁_, arg₂_)
+            body_
+        end                        |
+
+        (arg₁_, arg₂_) -> body_
+
+    ) || error("syntax: expected an anonymous function")
+
+    return :($InfixFunction($operator)) |> esc
+end
+
 
 end # module
