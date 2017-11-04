05e62f2be99e8239db6137bb36dc4a8f3b9e7a09
Ismael Venegas Castelló <ismael.vc1337@gmail.com>
2017-11-03 21:37:27 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using InfixFunctions
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+
+add = @infix (x, y) -> x + y
+
+sub = @infix function (x, y)
+    return x - y
+end
+
+x = 5
+
+@test x |add| x == 10
+@test x |sub| x == 0
